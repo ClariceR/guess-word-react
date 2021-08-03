15667aa9f7fc7155e53e0dba7089237d38864b43
clarice_r <claricerdallorto@gmail.com>
2021-08-03 14:43:18 +0100

    Added Header

--- src/AppBkp.js
+++ src/AppBkp.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Loading from "./Loading";
-import Header from "./Header";
-import './App.css';
+import Game from "./Game";
+import "./App.css";
 
 const url =
   "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt";
@@ -17,10 +17,9 @@ function App() {
       const wordsArray = words.split("\n");
       let randomIndex = Math.floor(Math.random() * wordsArray.length);
       const randomWord = wordsArray[randomIndex].trim();
-      setLoading(false)
+      setLoading(false);
       setWord(randomWord.toUpperCase());
       console.log(`From fetchWord we got: ${randomWord}`);
-      console.log('type of randomword: ' + typeof randomWord);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -31,23 +30,17 @@ function App() {
     fetchWord();
   }, []);
 
-  if(loading) {
+  if (loading) {
     return (
       <main>
         <Loading />
       </main>
-    )
+    );
   }
 
   return (
     <>
-      <section className="grid">
-        <div className="container flex-column f-jc-c">
-          <div className="wrapper flex-column f-jc-sb">
-            <Header />
-          </div>
-        </div>
-      </section>
+      <Game word={word} fetchWord={fetchWord} />
     </>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h2 className="title">
+      Guess <span>the</span> Word
+    </h2>
+  );
+}
+
+export default Header;
