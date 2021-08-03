89388ef4021e828e098da0d4b719f11c5948a52f
clarice_r <claricerdallorto@gmail.com>
2021-08-03 15:08:39 +0100

    Added component to give a feedback message

---
+++ src/FeedbackMessage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FeedbackMessage() {
+  return (
+    <div className="reserved-place">
+      <p className="feedback"></p>
+    </div>
+  );
+}
+
+export default FeedbackMessage;
