386cff5e1bc8d7904af321647281228d2e483a25
clarice_r <claricerdallorto@gmail.com>
2021-08-03 14:46:36 +0100

    Added game instructions

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function Header() {
   return (
-    <h2 className="title">
-      Guess <span>the</span> Word
-    </h2>
+    <>
+      <h2 className="title">
+        Guess <span>the</span> Word
+      </h2>
+      <p className="instructions">Enter a letter</p>
+    </>
   );
 }
 
